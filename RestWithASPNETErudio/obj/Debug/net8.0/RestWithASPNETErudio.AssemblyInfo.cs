26f70f31a8ed891549ed2eef196be655729321b7
GitHub Action <action@github.com>
2024-10-09 22:21:53 +0000

    Increment version to 8.0.303.3

--- RestWithASPNETErudio/obj/Debug/net8.0/RestWithASPNETErudio.AssemblyInfo.cs
+++ RestWithASPNETErudio/obj/Debug/net8.0/RestWithASPNETErudio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestWithASPNETErudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e1136015dd15b13a2c90239b8fae03c747e6a70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9afff7d0a4c8b4dc668a57ce13e4dd86f5653559")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestWithASPNETErudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestWithASPNETErudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
